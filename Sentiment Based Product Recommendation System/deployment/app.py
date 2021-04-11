96b7b39dae5d73241e76587768c1c6295f1396b5
Darshjyot Nagi <darshjyot.nagi@gmail.com>
2021-04-12 05:24:15 +0530

    1. Initial commit containing Jupyter notebook and deployment code

---
+++ Sentiment Based Product Recommendation System/deployment/app.py
@@ -0,0 +1,52 @@
+
+"""
+@author - Darshjyot
+"""
+
+from flask import Flask, render_template, request, redirect, url_for
+import pandas as pd
+import numpy as np
+import pickle
+import warnings
+import model
+from model import predict
+warnings.filterwarnings("ignore")
+
+app = Flask(__name__)  # intitialize the flaks app  # common 
+
+#Read metadata file
+product_info = pd.read_csv('dataset/sample30.csv', usecols = ['id', 'name', 'brand', 'categories'])
+product_info.drop_duplicates(subset='id', keep='first', inplace=True)
+product_info = product_info.set_index('id')
+
+# http:baseurl/recommendations/{username}
+
+@app.route('/recommendations/<username>', methods=['GET'])
+def age_pred(username):
+    # call the model for prediction
+    prediction = predict(username)
+    df_pred = pd.DataFrame(prediction, columns=['id'])
+
+    # Set product information
+    df_pred['name'] = df_pred['id'].apply(lambda x : str(product_info.loc[str(x), 'name']))
+    df_pred['brand'] = df_pred['id'].apply(lambda x : str(product_info.loc[str(x), 'brand']))
+    df_pred['categories'] = df_pred['id'].apply(lambda x : str(product_info.loc[str(x), 'categories']))
+    
+    return  render_template('view.html',tables=[df_pred.to_html(classes='product')], titles = ['Id', 'Name', 'Brand', 'Category'])
+
+
+
+
+
+# Any HTML template in Flask App render_template
+
+if __name__ == '__main__' :
+    app.run(debug=True )  # this command will enable the run of your flask app or api
+    
+    #,host="0.0.0.0")
+
+
+
+
+
+
